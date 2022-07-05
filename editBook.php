5e1491bdde4a492a6a1405631909d2b3567f8206
salma-ehab <salma.ehabg@gmail.com>
2022-07-05 18:14:49 +0200

    added files

--- editBook.php
+++ editBook.php
@@ -1,7 +1,15 @@
 <?php
 session_start();
-$connect = mysqli_connect('localhost','root','');
-mysqli_select_db($connect,'librarydb');
+
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+$connect = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+mysqli_select_db($connect,'heroku_1d9bb2bfffaebd9');
 
 $bookTitle = $_GET['selectedBookTitle'];
 $s = "select * from bookstable where title = '$bookTitle'";
